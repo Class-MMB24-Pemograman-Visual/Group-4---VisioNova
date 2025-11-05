7587a7cf28798957c86e47523ee64ef698c4c1be
Muhammad Marshal Putra Al Barry_5124521024 <marshal.armosa@gmail.com>
2025-11-05 09:58:48 +0700

    Prototype 4 dan Challenge 4

---
+++ Prototype 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal"); // pastikan Player Goal ada
+    }
+
+    void FixedUpdate()
+    {
+        if (playerGoal != null)
+        {
+            Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+            enemyRb.AddForce(lookDirection * speed); // gunakan FixedUpdate & jangan dikalikan deltaTime
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Enemy Goal" || other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
