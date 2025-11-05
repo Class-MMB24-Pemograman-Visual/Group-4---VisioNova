5ddae3afd05e5ba7eadbe12b81f4301fbb2ec11f
Muhammad Marshal Putra Al Barry_5124521024 <marshal.armosa@gmail.com>
2025-11-05 23:03:27 +0700

    Challenge 3 Visionova

---
+++ Prototype 3 Misi Sound dan Efek/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver = false;
+
+    public float floatForce = 3f;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity = new Vector3(0, -9.81f * gravityModifier, 0);
+        playerAudio = GetComponent<AudioSource>();
+
+        // Dorongan kecil di awal (bisa dikurangi kalau terlalu naik)
+        playerRb.AddForce(Vector3.up * 1, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        // Saat spasi ditekan dan belum game over, balon naik
+        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+        }
+    }
+}
